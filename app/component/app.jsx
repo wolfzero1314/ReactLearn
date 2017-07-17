5ae5028114e6f131dff562c5c6499f47ec4c1a75
wolfzero1314 <77227370@163.com>
2017-07-17 17:27:21 +0800

    app

---
+++ app/component/app.jsx
@@ -0,0 +1,13 @@
+//Component1.jsx
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Hello World!</div>
+    )
+    }
+}
+
+//导出组件
+export default App;
---
+++ app/main.js
@@ -0,0 +1,9 @@
+//main.js
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './component/app.jsx';
+
+ReactDom.render(
+    <App />,
+    document.getElementById('content')
+);
